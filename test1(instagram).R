6be1c359166e738ac4d4c257579eddb18fde11e6
kdragonkorea <morenmore6340@gmail.com>
2021-03-16 23:46:35 +0900

    add craling reply vector

--- test1(instagram).R
+++ test1(instagram).R
@@ -35,23 +35,56 @@ tag1 <- remDr$findElement(using='css selector','div._8MQSO.Cx7Bp > div > div > d
 tag1$clickElement()
 Sys.sleep(2)
 
+id <- NULL;txt <- NULL;date <- NULL;like <- NULL;reply <- NULL;reply_css <- NULL
 # 최근 게시물
-for (i in 1:4) {
-  
-  }
-  div:nth-child(1) > a > div > div._9AhH0
-div:nth-child(2) > a > div > div._9AhH0
-div:nth-child(3) > a > div > div._9AhH0
+post <- remDr$findElement(using='css selector','div:nth-child(3) > div > div:nth-child(1) > div:nth-child(1) > a > div > div._9AhH0')
+post$clickElement()
+# 2번째: div:nth-child(3) > div > div:nth-child(1) > div:nth-child(2) > a > div > div._9AhH0
+Sys.sleep(2)
 
 # id
-body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > div.EtaWk > ul > div > li > div > div > div.C4VMK > h2 > div > span > a
+id_css <- remDr$findElement(using='css selector','div.e1e1d > span > a')
+id_text <- id_css$getElementText()
+id <- c(id, unlist(id_text))
+Sys.sleep(2)
 
 # txt
-body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > div.EtaWk > ul > div > li > div > div > div.C4VMK > span
-
-# like
-body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.EDfFK.ygqzn > div > div > a
+txt_css <- remDr$findElement(using='css selector','div.eo2As > div.EtaWk > ul > div > li > div > div > div.C4VMK > span')
+txt_text <- txt_css$getElementText()
+txt <- c(txt, unlist(txt_text))
+Sys.sleep(2)
 
 # date
-body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > div.k_Q0X.I0_K8.NnvRN > a > time
+date_css <- remDr$findElement(using='css selector','div.C4VMK > div > div > time')
+date_text <- date_css$getElementAttribute("title")
+date <- c(date, unlist(date_text))
+Sys.sleep(2)
+
+# like
+like_css <- remDr$findElement(using='css selector','div.eo2As > section.EDfFK.ygqzn > div > div > a')
+like_text <- like_css$getElementText()
+like <- c(like, unlist(like_text))
+Sys.sleep(2)
+
+# reply, 첫 댓글은 (2) 부터 시작
+reply_css <- remDr$findElement(using='css selector','ul:nth-child(2) > div > li > div > div.C7I1f > div.C4VMK > span')
+
+if (reply_text == ''){
+  cat("no reply")
+  reply <- NA
+}else{
+  reply_css <- remDr$findElement(using='css selector','ul:nth-child(2) > div > li > div > div.C7I1f > div.C4VMK > span')
+  reply_text <- reply_css$getElementText()
+  reply <- c(reply, unlist(reply_text))
+  Sys.sleep(2)
+}
+
+# x버튼
+xbtn <- remDr$findElement(using='css selector','div.Igw0E.IwRSH.eGOV_._4EzTm.BI4qX.qJPeX.fm1AK.TxciK.yiMZG > button')
+xbtn$clickElement()
+
+df <- data.frame(id, txt, date, like, reply)
+View(df)
+
+
 
